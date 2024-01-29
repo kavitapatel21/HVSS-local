ffdd868d6c37d8cc0dc58326e76ae42d4210feec
kavitapatel21 <kavita.patel@weservecodes.com>
2024-01-29 15:46:59 +0530

    login/register validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,93 +1,97 @@
 import { Formik, Form, Field } from 'formik';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-      <div className="login-wrapper d-flex align-items-center justify-content-center"> 
-        <div className="container">
-          <div className='primary-title text-center text-white'>
-            HVSS
-          </div>
-          <div className="white-box">
-            <h1 className="secondary-title mb-5">Login</h1>
-            <Formik
-              // initialValues={{ email: '', password: '' }}
-              // validate={values => {
-              //   const errors = {};
-              //   if (!values.email) {
-              //     errors.email = 'Required';
-              //   } else if (
-              //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-              //   ) {
-              //     errors.email = 'Invalid email address';
-              //   }
-              //   return errors;
-              // }}
-              // onSubmit={(values, { setSubmitting }) => {
-              //   setTimeout(() => {
-              //     alert(JSON.stringify(values, null, 2));
-              //     setSubmitting(false);
-              //   }, 400);
-              // }}
-            >
-              {({
-                // values,
-                // errors,
-                // touched,
-                // handleChange,
-                // handleBlur,
-                // handleSubmit,
-                // isSubmitting,
-                /* and other goodies */
-              }) => (
-                <Form 
-                // onSubmit={handleSubmit}
-                >
-                  <div className='form-group mb-4'>
-                    <label for="email" className='label-title mb-2 d-block w-100 text-left'>Email</label>
-                    <Field 
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      // onChange={handleChange}
-                      // onBlur={handleBlur}
-                      // value={values.email}
-                    />
-                    {/* {errors.email && touched.email && errors.email} */}
-                  </div>
-                  <div className='form-group mb-4'>
-                    <label for="email" className='label-title mb-2 d-block w-100 text-left'>Password</label>
-                    <Field 
-                      type="password"
-                      name="password"
-                      placeholder="Password"
-                      // onChange={handleChange}
-                      // onBlur={handleBlur}
-                      // value={values.password}
-                    />
-                    {/* {errors.password && touched.password && errors.password} */}
-                  </div>
-                  <div className='text-end mb-4'>
-                    <span className='regular-title c-pointer'>Forgot password?</span>
-                  </div>
-                  <div className='form-group mb-4'>
-                    <button type="submit" 
-                      // disabled={isSubmitting}
-                      name="login"
-                      id='login'
-                      className='primary-button mx-auto'
-                    >
-                      Login
-                    </button>
-                  </div>
-                  <div className='text-center'>
-                    <span className='regular-title c-pointer'>New user? Create account</span>
-                  </div>
-                </Form>
-              )}
-            </Formik>
-          </div>
+      <div className="login-wrapper d-flex align-items-center justify-content-center">
+      <div className="container">
+        <div className='primary-title text-center text-white'>
+          HVSS
+        </div>
+        <div className="white-box">
+          <h1 className="secondary-title mb-5">Login</h1>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={values => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = 'Email is Required';
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = 'Invalid email address';
+              }
+
+              if (!values.password) {
+                errors.password = 'Password is Required';
+              }
+
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+            }) => (
+              <Form onSubmit={handleSubmit}>
+                <div className='form-group mb-4'>
+                  <label htmlFor="email" className='label-title mb-2 d-block w-100 text-left'>Email</label>
+                  <Field 
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                  />
+                  {errors.email && touched.email && <div className="error-message">{errors.email}</div>}
+                </div>
+                <div className='form-group mb-4'>
+                  <label htmlFor="password" className='label-title mb-2 d-block w-100 text-left'>Password</label>
+                  <Field 
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                  />
+                  {errors.password && touched.password && <div className="error-message">{errors.password}</div>}
+                </div>
+                <div className='text-end mb-4'>
+                  <span className='regular-title c-pointer'>Forgot password?</span>
+                </div>
+                <div className='form-group mb-4'>
+                  <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    name="login"
+                    id='login'
+                    className='primary-button mx-auto'
+                  >
+                    Login
+                  </button>
+                </div>
+                <div className='text-center'>
+                  <span className='regular-title c-pointer'>New user? <Link to="/register">Create account</Link></span>
+                </div>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
+    </div>
     );
   };
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,125 +1,93 @@
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage  } from 'formik';
 import {Row, Col} from 'react-bootstrap';
+import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
+
+const validationSchema = Yup.object({
+  firstname: Yup.string().required('First name is required'),
+  lastname: Yup.string().required('Last name is required'),
+  email: Yup.string().email('Invalid email address').required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
 const Register = () => {
     return (
-      <div className="login-wrapper d-flex align-items-center justify-content-center"> 
-        <div className="container">
-          <div className='primary-title text-center text-white'>
-            HVSS
-          </div>
-          <div className="white-box">
-            <h1 className="secondary-title mb-5">Create your account</h1>
-            <Formik
-              // initialValues={{ email: '', password: '' }}
-              // validate={values => {
-              //   const errors = {};
-              //   if (!values.email) {
-              //     errors.email = 'Required';
-              //   } else if (
-              //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-              //   ) {
-              //     errors.email = 'Invalid email address';
-              //   }
-              //   return errors;
-              // }}
-              // onSubmit={(values, { setSubmitting }) => {
-              //   setTimeout(() => {
-              //     alert(JSON.stringify(values, null, 2));
-              //     setSubmitting(false);
-              //   }, 400);
-              // }}
-            >
-              {({
-                // values,
-                // errors,
-                // touched,
-                // handleChange,
-                // handleBlur,
-                // handleSubmit,
-                // isSubmitting,
-                /* and other goodies */
-              }) => (
-                <Form 
-                // onSubmit={handleSubmit}
-                >
-                  <Row>
-                    <Col md={6}>
-                      <div className='form-group mb-4'>
-                        <label for="firstname" className='label-title mb-2 d-block w-100 text-left'>First name</label>
-                        <Field 
-                          type="text"
-                          name="firstname"
-                          placeholder="First Name"
-                          // onChange={handleChange}
-                          // onBlur={handleBlur}
-                          // value={values.firstname}
-                        />
-                        {/* {errors.firstname && touched.firstname && errors.firstname} */}
-                      </div>
-                    </Col>
-                    <Col md={6}>
-                      <div className='form-group mb-4'>
-                        <label for="lastname" className='label-title mb-2 d-block w-100 text-left'>Last name</label>
-                        <Field 
-                          type="text"
-                          name="lastname"
-                          placeholder="Last Name"
-                          // onChange={handleChange}
-                          // onBlur={handleBlur}
-                          // value={values.lastname}
-                        />
-                        {/* {errors.lastname && touched.lastname && errors.lastname} */}
-                      </div>
-                    </Col>
-                    <Col md={12}>
-                      <div className='form-group mb-4'>
-                        <label for="email" className='label-title mb-2 d-block w-100 text-left'>Email</label>
-                        <Field 
-                          type="email"
-                          name="email"
-                          placeholder="Email"
-                          // onChange={handleChange}
-                          // onBlur={handleBlur}
-                          // value={values.email}
-                        />
-                        {/* {errors.email && touched.email && errors.email} */}
-                      </div>
-                    </Col>
-                    <Col md={12}>
-                      <div className='form-group mb-4'>
-                        <label for="email" className='label-title mb-2 d-block w-100 text-left'>Password</label>
-                        <Field 
-                          type="password"
-                          name="password"
-                          placeholder="Password"
-                          // onChange={handleChange}
-                          // onBlur={handleBlur}
-                          // value={values.password}
-                        />
-                        {/* {errors.password && touched.password && errors.password} */}
-                      </div> 
-                    </Col>
-                  </Row>
+      <div className="login-wrapper d-flex align-items-center justify-content-center">
+      <div className="container">
+        <div className='primary-title text-center text-white'>
+          HVSS
+        </div>
+        <div className="white-box">
+          <h1 className="secondary-title mb-5">Create your account</h1>
+          <Formik
+            initialValues={{ firstname: '', lastname: '', email: '', password: '' }}
+            validationSchema={validationSchema}
+            onSubmit={(values, { setSubmitting }) => {
+              // Perform your submission logic here
+              console.log(values);
+              setSubmitting(false);
+            }}
+          >
+            <Form>
+              <Row>
+                <Col md={6}>
                   <div className='form-group mb-4'>
-                    <button type="submit" 
-                      // disabled={isSubmitting}
-                      name="login"
-                      id='login'
-                      className='primary-button mx-auto'
-                    >
-                      Create Account
-                    </button>
+                    <label htmlFor="firstname" className='label-title mb-2 d-block w-100 text-left'>First name</label>
+                    <Field 
+                      type="text"
+                      name="firstname"
+                      placeholder="First Name"
+                    />
+                    <ErrorMessage name="firstname" component="div" className="error-message" />
                   </div>
-                  <div className='text-center'>
-                    <span className='regular-title c-pointer'>Already user? Login</span>
+                </Col>
+                <Col md={6}>
+                  <div className='form-group mb-4'>
+                    <label htmlFor="lastname" className='label-title mb-2 d-block w-100 text-left'>Last name</label>
+                    <Field 
+                      type="text"
+                      name="lastname"
+                      placeholder="Last Name"
+                    />
+                    <ErrorMessage name="lastname" component="div" className="error-message" />
                   </div>
-                </Form>
-              )}
-            </Formik>
-          </div>
+                </Col>
+                <Col md={12}>
+                  <div className='form-group mb-4'>
+                    <label htmlFor="email" className='label-title mb-2 d-block w-100 text-left'>Email</label>
+                    <Field 
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                    />
+                    <ErrorMessage name="email" component="div" className="error-message" />
+                  </div>
+                </Col>
+                <Col md={12}>
+                  <div className='form-group mb-4'>
+                    <label htmlFor="password" className='label-title mb-2 d-block w-100 text-left'>Password</label>
+                    <Field 
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                    />
+                    <ErrorMessage name="password" component="div" className="error-message" />
+                  </div> 
+                </Col>
+              </Row>
+              <div className='form-group mb-4'>
+                <button type="submit" className='primary-button mx-auto'>
+                  Create Account
+                </button>
+              </div>
+              <div className='text-center'>
+                <span className='regular-title c-pointer'>Already a user?<Link to="/login">Login</Link></span>
+              </div>
+            </Form>
+          </Formik>
         </div>
       </div>
+    </div>
     );
   };
 export default Register;
