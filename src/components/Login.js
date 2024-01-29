e08da8762fc109eae4a0c72cb6b2e395e4e43730
kavitapatel21 <kavita.patel@weservecodes.com>
2024-01-29 16:50:08 +0530

    login/register validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,99 +1,4 @@
 import { Formik, Form, Field } from 'formik';
-<<<<<<< Updated upstream
-import PasswordShow from "../assets/images/password-show.svg";
-import PasswordHide from "../assets/images/password-hide.svg";
-
-const Login = () => {
-    return (
-      <div className="login-wrapper d-flex align-items-center justify-content-center"> 
-        <div className="container">
-          <div className='primary-title text-center text-white'>
-            HVSS
-          </div>
-          <div className="white-box">
-            <h1 className="secondary-title mb-5">Login</h1>
-            <Formik
-              // initialValues={{ email: '', password: '' }}
-              // validate={values => {
-              //   const errors = {};
-              //   if (!values.email) {
-              //     errors.email = 'Required';
-              //   } else if (
-              //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-              //   ) {
-              //     errors.email = 'Invalid email address';
-              //   }
-              //   return errors;
-              // }}
-              // onSubmit={(values, { setSubmitting }) => {
-              //   setTimeout(() => {
-              //     alert(JSON.stringify(values, null, 2));
-              //     setSubmitting(false);
-              //   }, 400);
-              // }}
-            >
-              {({
-                // values,
-                // errors,
-                // touched,
-                // handleChange,
-                // handleBlur,
-                // handleSubmit,
-                // isSubmitting,
-                /* and other goodies */
-              }) => (
-                <Form 
-                // onSubmit={handleSubmit}
-                >
-                  <div className='form-group mb-4'>
-                    <label htmlFor="email" className='label-title mb-2 d-block w-100 text-left'>Email</label>
-                    <Field 
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      // onChange={handleChange}
-                      // onBlur={handleBlur}
-                      // value={values.email}
-                    />
-                    {/* {errors.email && touched.email && errors.email} */}
-                  </div>
-                  <div className='form-group mb-4'>
-                    <label htmlFor="email" className='label-title mb-2 d-block w-100 text-left'>Password</label>
-                    <div className='position-relative'>
-                      <Field 
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        // onChange={handleChange}
-                        // onBlur={handleBlur}
-                        // value={values.password}
-                      />
-                      <img src={PasswordShow} className='ico_float right' alt='Password' />
-                      {/* <img src={PasswordHide} className='ico_float right' alt='Password' /> */}
-                    </div>
-                    {/* {errors.password && touched.password && errors.password} */}
-                  </div>
-                  <div className='text-end mb-4'>
-                    <span className='regular-title c-pointer'>Forgot password?</span>
-                  </div>
-                  <div className='form-group mb-4'>
-                    <button type="submit" 
-                      // disabled={isSubmitting}
-                      name="login"
-                      id='login'
-                      className='primary-button mx-auto'
-                    >
-                      Login
-                    </button>
-                  </div>
-                  <div className='text-center'>
-                    <span className='regular-title c-pointer'>New user? Create account</span>
-                  </div>
-                </Form>
-              )}
-            </Formik>
-          </div>
-=======
 import { Link } from 'react-router-dom';
 
 const Login = () => {
@@ -184,7 +89,6 @@ const Login = () => {
               </Form>
             )}
           </Formik>
->>>>>>> Stashed changes
         </div>
       </div>
     </div>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,7 @@
 import { Formik, Form, Field, ErrorMessage  } from 'formik';
 import {Row, Col} from 'react-bootstrap';
-<<<<<<< Updated upstream
-import PasswordShow from "../assets/images/password-show.svg";
-import PasswordHide from "../assets/images/password-hide.svg";
-=======
 import * as Yup from 'yup';
 import { Link } from 'react-router-dom';
->>>>>>> Stashed changes
 
 
 const validationSchema = Yup.object({
@@ -17,110 +12,6 @@ const validationSchema = Yup.object({
 });
 const Register = () => {
     return (
-<<<<<<< Updated upstream
-      <div className="login-wrapper d-flex align-items-center justify-content-center"> 
-        <div className="container">
-          <div className='primary-title text-center text-white'>
-            HVSS
-          </div>
-          <div className="white-box">
-            <h1 className="secondary-title mb-5">Create your account</h1>
-            <Formik
-              // initialValues={{ email: '', password: '' }}
-              // validate={values => {
-              //   const errors = {};
-              //   if (!values.email) {
-              //     errors.email = 'Required';
-              //   } else if (
-              //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-              //   ) {
-              //     errors.email = 'Invalid email address';
-              //   }
-              //   return errors;
-              // }}
-              // onSubmit={(values, { setSubmitting }) => {
-              //   setTimeout(() => {
-              //     alert(JSON.stringify(values, null, 2));
-              //     setSubmitting(false);
-              //   }, 400);
-              // }}
-            >
-              {({
-                // values,
-                // errors,
-                // touched,
-                // handleChange,
-                // handleBlur,
-                // handleSubmit,
-                // isSubmitting,
-                /* and other goodies */
-              }) => (
-                <Form 
-                // onSubmit={handleSubmit}
-                >
-                  <Row>
-                    <Col md={6}>
-                      <div className='form-group mb-4'>
-                        <label htmlFor="firstname" className='label-title mb-2 d-block w-100 text-left'>First name</label>
-                        <Field 
-                          type="text"
-                          name="firstname"
-                          placeholder="First Name"
-                          // onChange={handleChange}
-                          // onBlur={handleBlur}
-                          // value={values.firstname}
-                        />
-                        {/* {errors.firstname && touched.firstname && errors.firstname} */}
-                      </div>
-                    </Col>
-                    <Col md={6}>
-                      <div className='form-group mb-4'>
-                        <label htmlFor="lastname" className='label-title mb-2 d-block w-100 text-left'>Last name</label>
-                        <Field 
-                          type="text"
-                          name="lastname"
-                          placeholder="Last Name"
-                          // onChange={handleChange}
-                          // onBlur={handleBlur}
-                          // value={values.lastname}
-                        />
-                        {/* {errors.lastname && touched.lastname && errors.lastname} */}
-                      </div>
-                    </Col>
-                    <Col md={12}>
-                      <div className='form-group mb-4'>
-                        <label htmlFor="email" className='label-title mb-2 d-block w-100 text-left'>Email</label>
-                        <Field 
-                          type="email"
-                          name="email"
-                          placeholder="Email"
-                          // onChange={handleChange}
-                          // onBlur={handleBlur}
-                          // value={values.email}
-                        />
-                        {/* {errors.email && touched.email && errors.email} */}
-                      </div>
-                    </Col>
-                    <Col md={12}>
-                      <div className='form-group mb-4'>
-                        <label htmlFor="email" className='label-title mb-2 d-block w-100 text-left'>Password</label>
-                        <div className='position-relative'>
-                          <Field 
-                            type="password"
-                            name="password"
-                            placeholder="Password"
-                            // onChange={handleChange}
-                            // onBlur={handleBlur}
-                            // value={values.password}
-                          />
-                          <img src={PasswordShow} className='ico_float right' alt='Password' />
-                        {/* <img src={PasswordHide} className='ico_float right' alt='Password' /> */}
-                        </div>
-                        {/* {errors.password && touched.password && errors.password} */}
-                      </div> 
-                    </Col>
-                  </Row>
-=======
       <div className="login-wrapper d-flex align-items-center justify-content-center">
       <div className="container">
         <div className='primary-title text-center text-white'>
@@ -151,7 +42,6 @@ const Register = () => {
                   </div>
                 </Col>
                 <Col md={6}>
->>>>>>> Stashed changes
                   <div className='form-group mb-4'>
                     <label htmlFor="lastname" className='label-title mb-2 d-block w-100 text-left'>Last name</label>
                     <Field 
